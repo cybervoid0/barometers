2c709d1d1b135d8de5078e36dc568e471b3be210
Alex <shenshin@me.com>
2024-09-08 19:09:12 +0200

    refactor: menu drawer

--- app/components/header/MobileMenu.tsx
+++ app/components/header/MobileMenu.tsx
@@ -26,107 +26,93 @@ export const MobileMenu: FC<DrawerProps> = props => {
   const toggle = (index: number) => setOpened(old => ({ ...old, [index]: !old[index] }))
 
   return (
-    <Drawer.Root
+    <Drawer
       size="sm"
       transitionProps={{
         duration: 500,
       }}
       {...props}
     >
-      <Drawer.Overlay />
-      <Drawer.Content>
-        <Drawer.Header>
-          <Drawer.CloseButton />
-        </Drawer.Header>
-
-        {/* Body */}
-        <Drawer.Body p={0} h="calc(100vh - 4rem)" style={{ overflow: 'hidden' }}>
-          <Stack h="100%" justify="space-between">
-            {/* Menu */}
-            <Box
-              component={motion.div}
-              initial={{
-                x: -100,
-              }}
-              animate={{
-                x: 0,
-              }}
-              transition={{ delay: 0.3, duration: 0.3 }}
-            >
-              <List px="xl" listStyleType="none">
-                {menuData.map((outer, i, arr) => (
-                  <Fragment key={outer.id}>
-                    {isSingleMenuItem(outer) ? (
-                      <List.Item py="md">
-                        <Anchor c="inherit" component={Link} href={outer.link}>
+      <Stack h="100%" justify="space-between">
+        {/* Menu */}
+        <Box
+          component={motion.div}
+          initial={{ x: -100 }}
+          animate={{ x: 0 }}
+          transition={{ delay: 0.3, duration: 0.3 }}
+        >
+          <List px="xl" listStyleType="none">
+            {menuData.map((outer, i, arr) => (
+              <Fragment key={outer.id}>
+                {isSingleMenuItem(outer) ? (
+                  <List.Item py="md">
+                    <Anchor c="inherit" component={Link} href={outer.link}>
+                      <Text size="sm" tt="uppercase" lts="0.15rem" fw={500}>
+                        {outer.label}
+                      </Text>
+                    </Anchor>
+                  </List.Item>
+                ) : (
+                  <>
+                    <List.Item py="md">
+                      <UnstyledButton onClick={() => toggle(i)}>
+                        <Group gap="sm">
                           <Text size="sm" tt="uppercase" lts="0.15rem" fw={500}>
                             {outer.label}
                           </Text>
-                        </Anchor>
-                      </List.Item>
-                    ) : (
-                      <>
-                        <List.Item py="md">
-                          <UnstyledButton onClick={() => toggle(i)}>
-                            <Group gap="sm">
-                              <Text size="sm" tt="uppercase" lts="0.15rem" fw={500}>
-                                {outer.label}
+                          <Center
+                            component={motion.div}
+                            animate={{ rotate: opened[i] ? 90 : 0 }}
+                            transition={{ duration: 0.3 }}
+                          >
+                            <Arrow />
+                          </Center>
+                        </Group>
+                      </UnstyledButton>
+                    </List.Item>
+                    <Collapse transitionDuration={500} in={opened[i]}>
+                      <List px="xl" listStyleType="none">
+                        {outer.children.map(inner => (
+                          <List.Item pb="sm" key={inner.id}>
+                            <Anchor c="inherit" component={Link} href={outer.link + inner.link}>
+                              <Text size="xs" tt="capitalize" lts="0.1rem" fw={400}>
+                                {inner.label}
                               </Text>
-                              <Center
-                                component={motion.div}
-                                animate={{ rotate: opened[i] ? 90 : 0 }}
-                                transition={{ duration: 0.3 }}
-                              >
-                                <Arrow />
-                              </Center>
-                            </Group>
-                          </UnstyledButton>
-                        </List.Item>
-                        <Collapse transitionDuration={500} in={opened[i]}>
-                          <List px="xl" listStyleType="none">
-                            {outer.children.map(inner => (
-                              <List.Item pb="sm" key={inner.id}>
-                                <Anchor c="inherit" component={Link} href={outer.link + inner.link}>
-                                  <Text size="xs" tt="capitalize" lts="0.1rem" fw={400}>
-                                    {inner.label}
-                                  </Text>
-                                </Anchor>
-                              </List.Item>
-                            ))}
-                          </List>
-                        </Collapse>
-                      </>
-                    )}
-                    {i < arr.length - 1 && <Divider />}
-                  </Fragment>
-                ))}
-              </List>
-            </Box>
+                            </Anchor>
+                          </List.Item>
+                        ))}
+                      </List>
+                    </Collapse>
+                  </>
+                )}
+                {i < arr.length - 1 && <Divider />}
+              </Fragment>
+            ))}
+          </List>
+        </Box>
 
-            {/* Footer */}
-            <Box
-              component={motion.div}
-              initial={{ y: 70 }}
-              animate={{ y: 0 }}
-              transition={{ delay: 0.3, duration: 0.3 }}
-            >
-              <Divider />
-              <Group h="4rem" align="center" justify="space-evenly">
-                <Anchor aria-label="Instagram" target="_blank" href={instagram} lh={0}>
-                  <ActionIcon variant="default" size="sm" bd="none">
-                    <SiInstagram size="100%" />
-                  </ActionIcon>
-                </Anchor>
-                <Anchor aria-label="Email" target="_blank" href={`mailto:${email}`} lh={0}>
-                  <ActionIcon variant="default" size="sm" bd="none">
-                    <SiMaildotru size="100%" />
-                  </ActionIcon>
-                </Anchor>
-              </Group>
-            </Box>
-          </Stack>
-        </Drawer.Body>
-      </Drawer.Content>
-    </Drawer.Root>
+        {/* Footer */}
+        <Box
+          component={motion.div}
+          initial={{ y: 70 }}
+          animate={{ y: 0 }}
+          transition={{ delay: 0.3, duration: 0.3 }}
+        >
+          <Divider />
+          <Group h="4rem" align="center" justify="space-evenly">
+            <Anchor aria-label="Instagram" target="_blank" href={instagram} lh={0}>
+              <ActionIcon variant="default" size="sm" bd="none">
+                <SiInstagram size="100%" />
+              </ActionIcon>
+            </Anchor>
+            <Anchor aria-label="Email" target="_blank" href={`mailto:${email}`} lh={0}>
+              <ActionIcon variant="default" size="sm" bd="none">
+                <SiMaildotru size="100%" />
+              </ActionIcon>
+            </Anchor>
+          </Group>
+        </Box>
+      </Stack>
+    </Drawer>
   )
 }
