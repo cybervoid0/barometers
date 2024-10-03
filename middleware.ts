3f85620b7ad0b10e5af742e50221e9c9ebfdad3d
Alex <shenshin@me.com>
2024-10-04 01:58:13 +0200

    debug: log token

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,23 @@
-import authMiddleware from 'next-auth/middleware'
+// import authMiddleware from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
+
+export default async function middleware(req: NextRequest) {
+  // Получаем JWT токен
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  console.log('Token:', token)
+  console.log('NEXTAUTH_SECRET', process.env.NEXTAUTH_SECRET)
+
+  // Если токена нет, перенаправляем на страницу логина
+  if (!token) {
+    return NextResponse.redirect(new URL('/signin', req.url))
+  }
+
+  // Если токен есть, продолжаем с запросом
+  return NextResponse.next()
+}
 
 // sets up protected routes
-export default authMiddleware
+//export default authMiddleware
 
-export const config = { matcher: ['/admin/'] }
+export const config = { matcher: ['/admin', '/admin/'] }
