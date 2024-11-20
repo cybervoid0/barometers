0f184e634f2b126376a687e503d0170362e2bda6
Alex <shenshin@me.com>
2024-11-20 22:58:15 +0100

    feat: add text indexing for name and description in Barometer and Manufacturer schemas

--- models/manufacturer.ts
+++ models/manufacturer.ts
@@ -42,6 +42,10 @@ const manufacturerSchema = new Schema<IManufacturer>(
     timestamps: true,
   },
 )
+manufacturerSchema.index({
+  name: 'text',
+  country: 'text',
+})
 
 const Manufacturer: Model<IManufacturer> =
   models?.Manufacturer || model<IManufacturer>('Manufacturer', manufacturerSchema)
