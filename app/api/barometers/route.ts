942db5198b71ad7fb476dc474ccb165bfad0e144
Cybervoid <shenshin@me.com>
2024-12-11 06:57:57 +0100

    feat: implement pagination functionality and API integration for barometers search

--- app/api/barometers/route.ts
+++ app/api/barometers/route.ts
@@ -5,76 +5,23 @@ import Barometer, { IBarometer } from '@/models/barometer'
 import BarometerType from '@/models/type'
 import '@/models/condition'
 import Manufacturer from '@/models/manufacturer'
-import { cleanObject, slug as slugify, parseDate } from '@/utils/misc'
+import { cleanObject, slug as slugify } from '@/utils/misc'
 import { SortValue } from '@/app/collection/types/[type]/types'
+import { DEFAULT_PAGE_SIZE, type PaginationDTO } from '../types'
 
-export interface PaginationDTO {
-  barometers: IBarometer[]
-  /**
-   * Total number of barometers in response before applying pagination
-   */
-  total: number
-  page: number
-}
-
-// dependencies to include in resulting barometers array
-const deps = ['type', 'condition', 'manufacturer']
-
-/**
- * Sort barometer list by one of the parameters: manufacturer, name, date or catalogue no.
- */
-function sortBarometers(barometers: IBarometer[], sortBy: SortValue | null): IBarometer[] {
-  return barometers.toSorted((a, b) => {
-    switch (sortBy) {
-      case 'manufacturer':
-        return (a.manufacturer?.name ?? '').localeCompare(b.manufacturer?.name ?? '')
-      case 'name':
-        return a.name.localeCompare(b.name)
-      case 'date': {
-        if (!a.dating || !b.dating) return 0
-        const yearA = parseDate(a.dating)?.[0]
-        const yearB = parseDate(b.dating)?.[0]
-        if (!yearA || !yearB) return 0
-        const dateA = new Date(yearA, 0, 1).getTime()
-        const dateB = new Date(yearB, 0, 1).getTime()
-        return dateA - dateB
-      }
-      case 'cat-no':
-        return a.collectionId.localeCompare(b.collectionId)
-      default:
-        return 0
-    }
-  })
-}
-
-/**
- * Search barometers matching a query
- */
-async function searchBarometers(
-  query: string,
-  page: number,
-  limit: number,
-  sortBy: SortValue | null,
-) {
-  const quotedQuery = `"${query}"`
-  const skip = (page - 1) * limit
-  const barometers = sortBarometers(
-    await Barometer.find({ $text: { $search: quotedQuery } })
-      .populate(deps)
-      .skip(skip)
-      .limit(limit),
-    sortBy,
-  )
-  const total = await Barometer.countDocuments({ $text: { $search: quotedQuery } })
-
-  return NextResponse.json<PaginationDTO>(
-    {
-      barometers,
-      total,
-      page,
-    },
-    { status: 200 },
-  )
+function getSortCriteria(sortBy: SortValue | null): Record<string, 1 | -1> {
+  switch (sortBy) {
+    case 'manufacturer':
+      return { 'manufacturer.name': 1 }
+    case 'name':
+      return { name: 1 }
+    case 'date':
+      return { dating: 1 }
+    case 'cat-no':
+      return { collectionId: 1 }
+    default:
+      return { name: 1 }
+  }
 }
 
 /**
@@ -83,7 +30,7 @@ async function searchBarometers(
 async function getBarometersByType(
   typeName: string,
   page: number,
-  limit: number,
+  pageSize: number,
   sortBy: SortValue | null,
 ) {
   // perform case-insensitive compare with the stored types
@@ -93,42 +40,58 @@ async function getBarometersByType(
   if (!barometerType) return NextResponse.json([], { status: 404 })
 
   // if existing barometer type match the `type` param, return all corresponding barometers
-  const skip = (page - 1) * limit
-  const barometers = sortBarometers(
-    await Barometer.find({ type: barometerType._id })
-      .skip(skip)
-      .limit(limit)
-      .populate(['type', 'condition', 'manufacturer']),
-    sortBy,
-  )
-  const total = await Barometer.countDocuments({ type: barometerType._id })
+  const skip = (page - 1) * pageSize
+  const sortCriteria = getSortCriteria(sortBy)
+
+  const barometers = await Barometer.aggregate([
+    {
+      $match: { type: barometerType._id },
+    },
+    {
+      $lookup: {
+        from: 'manufacturers',
+        localField: 'manufacturer',
+        foreignField: '_id',
+        as: 'manufacturer',
+      },
+    },
+    { $unwind: { path: '$manufacturer', preserveNullAndEmptyArrays: true } },
+    {
+      $lookup: {
+        from: 'barometerTypes',
+        localField: 'type',
+        foreignField: '_id',
+        as: 'type',
+      },
+    },
+    { $unwind: { path: '$type', preserveNullAndEmptyArrays: true } },
+    {
+      $lookup: {
+        from: 'barometerConditions',
+        localField: 'condition',
+        foreignField: '_id',
+        as: 'condition',
+      },
+    },
+    { $unwind: { path: '$condition', preserveNullAndEmptyArrays: true } },
+    {
+      $sort: sortCriteria,
+    },
+    { $skip: skip },
+    { $limit: pageSize },
+  ])
+
+  const totalItems = await Barometer.countDocuments({ type: barometerType._id })
+
   return NextResponse.json<PaginationDTO>(
     {
       barometers,
       page,
-      total,
+      totalItems,
+      pageSize,
+      totalPages: Math.ceil(totalItems / pageSize),
     },
     { status: barometers.length > 0 ? 200 : 404 },
-  )
-}
-
-/**
- * Find all barometers
- */
-async function getAllBarometers(page: number, limit: number, sortBy: SortValue | null) {
-  const skip = (page - 1) * limit
-  const barometers = sortBarometers(
-    await Barometer.find().skip(skip).limit(limit).populate(deps),
-    sortBy,
-  )
-  const total = await Barometer.countDocuments()
-  return NextResponse.json<PaginationDTO>(
-    {
-      barometers,
-      total,
-      page,
-    },
-    { status: 200 },
   )
 }
 
@@ -142,19 +105,11 @@ export async function GET(req: NextRequest) {
     await connectMongoose()
     const { searchParams } = req.nextUrl
     const typeName = searchParams.get('type')
+    if (!typeName) throw new Error('Unknown barometer type')
     const sortBy = searchParams.get('sort') as SortValue | null
-    const query = searchParams.get('q')
-    const limit = Math.max(Number(searchParams.get('limit')) || 0, 0)
-    console.log('🚀 ~ GET ~ limit:', limit)
+    const size = Math.max(Number(searchParams.get('pageSize')) || DEFAULT_PAGE_SIZE, 1)
     const page = Math.max(Number(searchParams.get('page')) || 1, 1)
-    console.log('🚀 ~ GET ~ page:', page)
-
-    // query param was received: ?q=aneroid%20barometer
-    if (query) return await searchBarometers(query, page, limit, sortBy)
-    // if `type` search param was not passed return all barometers list
-    if (!typeName || !typeName.trim()) return await getAllBarometers(limit, page, sortBy)
-    // type was passed
-    return await getBarometersByType(typeName, page, limit, sortBy)
+    return await getBarometersByType(typeName, page, size, sortBy)
   } catch (error) {
     return NextResponse.json(
       { message: error instanceof Error ? error.message : 'Could not retrieve barometer list' },
