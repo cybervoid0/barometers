0e37aacf109b61b9240f52351bdca090a29a660c
Cybervoid <shenshin@me.com>
2024-12-18 01:23:53 +0100

    refactor: update user registration to use Prisma client and improve error handling

--- actions/register.ts
+++ actions/register.ts
@@ -1,22 +1,24 @@
 'use server'
 
 import { hash } from 'bcrypt'
-import { connectMongoose } from '@/utils/mongoose'
-import User, { IUser } from '@/models/user'
+import type { User } from '@prisma/client'
+import { getPrismaClient } from '@/prisma/prismaClient'
 
-export const register = async (values: Partial<IUser>): Promise<void> => {
+export const register = async (values: Partial<User>): Promise<void> => {
   const { email, password, name } = values
   if (!password) throw new Error('Password is not defined')
   if (!email) throw new Error('Email is not defined')
   if (!name) throw new Error('Name is not defined')
-  await connectMongoose()
-  const userFound = await User.findOne({ email })
+  const prisma = getPrismaClient()
+  const userFound = await prisma.user.findUnique({ where: { email } })
   if (userFound) throw new Error('Email already exists!')
   const hashedPassword = await hash(password, 10)
-  const user = new User({
-    name,
-    email,
-    password: hashedPassword,
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      role: 'USER',
+    },
   })
-  await user.save()
 }
