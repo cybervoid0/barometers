4f2b47320cdb6830ccc1c4bc7dfe1cab693fcfab
Cybervoid <shenshin@me.com>
2025-03-21 22:03:33 +0100

    feat: add database backup functionality

---
+++ db-backup.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import { exec } from 'child_process'
+import { ensureDirSync } from 'fs-extra'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '.env') })
+
+const url =
+  process.env.DATABASE_URL_UNPOOLED ||
+  process.env.POSTGRES_URL_NON_POOLING ||
+  process.env.DATABASE_URL
+if (!url) throw new Error('No DATABASE_URL in .env')
+
+ensureDirSync('backups')
+
+const time = new Date().toISOString().replace(/[:T]/g, '-').split('.')[0]
+const projectRoot = path.resolve(__dirname)
+const backupDir = path.join(projectRoot, 'backups')
+const file = path.join(backupDir, `backup_${time}.dump`)
+
+exec(`/opt/homebrew/opt/libpq/bin/pg_dump "${url}" -F c -f "${file}"`, e =>
+  console.log(
+    e
+      ? `❌ Error: Unable to create Barometers DB dump. ${e instanceof Error ? e.message : e}`
+      : `✅ Barometers DB dump saved to ${file}`,
+  ),
+)
