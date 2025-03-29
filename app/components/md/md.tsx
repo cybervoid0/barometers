6edb8af5392481fd406c47ffb2a7c06766a320f5
Cybervoid <shenshin@me.com>
2025-03-29 06:23:50 +0100

    fix:  update Markdown component styles

--- app/components/md/md.tsx
+++ app/components/md/md.tsx
@@ -1,11 +1,26 @@
 import { FC } from 'react'
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown, { Components } from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-import clsx from 'clsx'
-import sx from './styles.module.scss'
+
+const defaultComponents: Components = {
+  p: props => <p className="mb-4 indent-4" {...props} />,
+  ul: props => <ul className="mb-4" {...props} />,
+  li: props => <li className="ml-8 list-disc" {...props} />,
+  a: props => (
+    <a
+      className="underline transition-colors duration-400 ease-out hover:text-amber-800"
+      {...props}
+    />
+  ),
+}
 
 export const MD: FC<Parameters<typeof ReactMarkdown>[0]> = ({ children, className, ...props }) => (
-  <ReactMarkdown {...props} className={clsx(className, sx.paragraph)} remarkPlugins={[remarkGfm]}>
+  <ReactMarkdown
+    {...props}
+    components={defaultComponents}
+    className={className}
+    remarkPlugins={[remarkGfm]}
+  >
     {children}
   </ReactMarkdown>
 )
