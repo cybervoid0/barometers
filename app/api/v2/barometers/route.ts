aa819047cea15e23353eb9ae11cfbc76fbe3d4cf
Cybervoid <shenshin@me.com>
2025-05-19 11:38:39 +0200

    refactor: add logging for category revalidation in PUT handler

--- app/api/v2/barometers/route.ts
+++ app/api/v2/barometers/route.ts
@@ -114,7 +114,8 @@ export const PUT = withPrisma(async (prisma, req: NextRequest) => {
     console.log('starting revalidation')
     revalidatePath(FrontRoutes.Barometer + newData.slug)
     console.log('revalidating category')
-    await revalidateCategory(prisma, newData.categoryId ?? categoryId)
+    console.log('skipping category revalidation newData.categoryId', newData.categoryId, categoryId)
+    //await revalidateCategory(prisma, newData.categoryId ?? categoryId)
     console.log('revalidated category')
     return NextResponse.json({ slug: newData.slug }, { status: 200 })
   } catch (error) {
