024b8acf3758f18c7717bae73e5579116abdc5c6
Cybervoid <mail@cybervoid.my>
2025-07-27 19:14:23 +0200

    feat: update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: `@radix-ui/react-slot`, `class-variance-authority`, `lucide-react`, `tailwind-merge`, and `tailwindcss-animate`.
    - Enhanced Tailwind CSS configuration with new color and border radius utilities.
    - Updated global styles to include CSS variables for theming and dark mode support.
    - Refactored the NewArrivals component to utilize the new Button component for improved styling.

--- app/components/new-arrivals/new-arrivals.tsx
+++ app/components/new-arrivals/new-arrivals.tsx
@@ -1,15 +1,14 @@
-import { Anchor, Button, Text } from '@mantine/core'
 import Link from 'next/link'
-import React from 'react'
 import { FrontRoutes } from '@/utils/routes-front'
+import { Button } from '@/components/ui/button'
 
 export function NewArrivals() {
   return (
-    <Anchor fw={600} fz="h4" href={FrontRoutes.NewArrivals} component={Link}>
-      <Button size="sm" lts="0.05rem" tt="uppercase" color="primary">
-        <Text visibleFrom="xs">New Arrivals</Text>
-        <Text hiddenFrom="xs">New</Text>
-      </Button>
-    </Anchor>
+    <Button asChild size="sm" className="uppercase tracking-wider">
+      <Link href={FrontRoutes.NewArrivals}>
+        <p className="hidden text-base xs:block">New Arrivals</p>
+        <p className="xs:hidden">New</p>
+      </Link>
+    </Button>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,71 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#473e36',
-        'page-bg': '#efefef',
-        'card-bg': '#dddddd',
-      },
-    },
-    screens: {
-      xs: '576px',
-      sm: '768px',
-      md: '992px',
-      lg: '1200px',
-      xl: '1400px',
-      xl2: '1600px',
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			'page-bg': '#efefef',
+  			'card-bg': '#dddddd',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	},
+  	screens: {
+  		xs: '576px',
+  		sm: '768px',
+  		md: '992px',
+  		lg: '1200px',
+  		xl: '1400px',
+  		xl2: '1600px'
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
