9466a3e4331bf5a32b56cf4caef1a211746fc35a
Cybervoid <mail@cybervoid.my>
2025-08-16 14:17:32 +0200

    style(page): update grid layout and header styles for consistency
    - Removed redundant grid style variable and directly applied grid classes in the HomePage component for clarity.
    - Simplified header height styles for a more consistent appearance across different screen sizes.
    - Adjusted gap and height properties in the header logo section for improved visual alignment.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { getCategories } from '@/services'
 import { cn } from '@/utils'
 
 export const dynamic = 'force-static'
-const gridStyle = 'grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3'
 
 export default async function HomePage() {
   const categories = await getCategories()
@@ -17,7 +16,7 @@ export default async function HomePage() {
         <div className="hidden md:block lg:col-span-3" />
         <SearchField className="col-span-4 md:col-span-3 lg:col-span-2" />
       </div>
-      <div className={cn(gridStyle, 'gap-4 sm:gap-5')}>
+      <div className={cn('xs:grid-cols-2 grid grid-cols-1 gap-4 sm:gap-5 lg:grid-cols-3')}>
         {categories.map(({ id, name, image }, i) => (
           <CategoryCard
             key={id}
--- components/containers/header/header.tsx
+++ components/containers/header/header.tsx
@@ -13,10 +13,10 @@ export async function Header() {
   const menu = await getMenuData()
   return (
     <>
-      <header className="h-[4.6rem] min-h-[4.6rem] sm:h-24 sm:min-h-24">
+      <header className="h-24 min-h-24">
         <div
           className={cn(
-            'fixed top-0 z-50 h-[4.6rem] min-h-[4.6rem] w-full sm:h-24 sm:min-h-24',
+            'fixed top-0 z-50 h-24 min-h-24 w-full',
             'from-layout-gradient-from via-layout-gradient-to to-layout-gradient-to bg-linear-to-t',
           )}
         >
@@ -27,14 +27,14 @@ export async function Header() {
             </div>
             <NextLink className="no-underline" href="/">
               <div className="flex items-center gap-4">
-                <div className="flex flex-col gap-[0.32rem]">
+                <div className="flex flex-col gap-2">
                   <h1 className="font-cinzel leading-none">Barometers Realm</h1>
                   <p className="xs:text-xs text-[10px] leading-none lg:text-[0.875rem]">
                     Europe's richest online collection of barometers
                   </p>
                 </div>
                 {/* Logo image */}
-                <div className="relative aspect-square h-10 sm:h-12">
+                <div className="relative aspect-square h-12">
                   <NextImage
                     unoptimized
                     fill
